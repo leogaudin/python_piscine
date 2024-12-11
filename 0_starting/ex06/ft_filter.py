6e98a6fd23413351ea08ae3e80d102a259e92e87
Léo <leogaudinoff@gmail.com>
2024-12-11 15:08:59 +0100

    fix: equivalence with original __doc__

--- 0_starting/ex06/ft_filter.py
+++ 0_starting/ex06/ft_filter.py
@@ -2,9 +2,9 @@ def ft_filter(
     function,
     iterable,
 ):
-    """Return an iterator yielding those items of iterable for which
-    function(item) is true. If function is None, return the items that
-    are true."""
+    """Return an iterator yielding those items of iterable for which function\
+(item)
+    is true. If function is None, return the items that are true."""
     if function is not None:
         return iter([item for item in iterable if function(item)])
 
